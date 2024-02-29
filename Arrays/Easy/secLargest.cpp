a932328ab767da23bef767ff3f2193450d725b7d
dhiman64 <dhimanikshit64@gmail.com>
2024-02-29 15:39:37 +0530

    Basic 01

--- Arrays/Easy/secLargest.cpp
+++ Arrays/Easy/secLargest.cpp
@@ -12,6 +12,11 @@ int secLargest(int arr[] , int n)
 */
 
 
+/* 
+    Method 1 :- Using Double iterations 
+            T(N) :- O(2N)
+
+
 int largest(int arr[] , int n)
 {
     sort(arr,arr+n);
@@ -32,6 +37,33 @@ int secLargest(int arr[] , int n)
     }
     return secMaxi;
 }
+
+*/
+
+
+// Optimal Approach :- Using Single Iteration
+
+    // T(N) = O(N)
+
+int secLargest(int arr[] , int n)
+{
+    int largest = arr[0];
+    int secLar = INT_MIN;
+    for(int i=0 ; i<n ; i++)
+    {
+        if(arr[i] > largest)
+        {
+            secLar = largest;
+            largest = arr[i]
+        }
+        else if(arr[i] < largest && arr[i] > secLar)
+        {
+            secLar = arr[i];
+        }
+    }
+    return secLar;
+}
+
 
 
 int main()
